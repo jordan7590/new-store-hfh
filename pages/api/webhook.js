6f62bfc5d01d688b949d5c28d5cd703b540c3665
Jordan Bell <jordan@tonserve.com>
2024-05-11 23:06:52 -0400

    order data added to webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -44,8 +44,50 @@ export default async function handler(req, res) {
 
         // Construct order data for WooCommerce
         const orderData = {
-          // Your order data here
-        };
+            payment_method: "stripe",
+            payment_method_title: "Card",
+            set_paid: true,
+            billing: {
+              first_name: "John",
+              last_name: "Doe",
+              address_1: "969 Market",
+              address_2: "",
+              city: "San Francisco",
+              state: "CA",
+              postcode: "94103",
+              country: "US",
+              email: "john.doe@example.com",
+              phone: "(555) 555-5555"
+            },
+            shipping: {
+              first_name: "John",
+              last_name: "Doe",
+              address_1: "969 Market",
+              address_2: "",
+              city: "San Francisco",
+              state: "CA",
+              postcode: "94103",
+              country: "US"
+            },
+            line_items: [
+              {
+                product_id: 93,
+                quantity: 2
+              },
+              {
+                product_id: 22,
+                variation_id: 23,
+                quantity: 1
+              }
+            ],
+            shipping_lines: [
+              {
+                method_id: "flat_rate",
+                method_title: "Flat Rate",
+                total: "10.00"
+              }
+            ]
+          };
 
         try {
           // Create order in WooCommerce using Axios
