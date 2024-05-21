6495c685dd5e34e194ebb80a72b71292e54bc7e5
Jordan Bell <jordan@tonserve.com>
2024-05-21 16:23:56 -0400

    added privacy-policy page

---
+++ pages/page/privacy-policy.js
@@ -0,0 +1,237 @@
+import React from "react";
+import CommonLayout from "../../components/shop/common-layout";
+import { Container, Row, Col, Media } from "reactstrap";
+
+
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <CommonLayout parent="home" title="Privacy Policy">
+        <div>
+     
+         <div className="bg-img main-img subpage"
+                   style={{
+                    backgroundImage: 'url(/assets/images/icon/privacy-header.jpg)',
+                    backgroundPositionY: '68%'
+                }}>
+                <Container>
+                    <div className="main-title text-bottom">
+                        <h1 className="text-gray title-text">Privacy Policy</h1>
+                    </div>
+                </Container>
+              </div>
+        </div>
+      
+        {/* // <!-- ReturnPolicy section start --> */}
+        <section className="policy-page section-b-space">
+          <Container>
+            <Row>
+              <Col lg="12">
+                <div className="policy-body">
+                    <ul>
+                      <li>
+                        <p>
+                          <strong><u>Acceptance of Terms.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>
+                              Welcome to <strong>www.medicallogowear.com</strong> (the "<strong>Site</strong>"), a web site operated on behalf of Hoyt & Company
+                               ("<strong>Hoyt & Company</strong>", "<strong>us</strong>" and "<strong>our</strong>"). Your privacy is important to us.
+                                It is our policy to protect your Personal Information (as defined below) and to use it only in accordance with this Privacy Policy.
+                                 Using our Site is voluntary, and by using the Site, you consent to our collection and use of your identifying and non-identifying information as
+                                  described in this Privacy Policy. This Privacy Policy only governs our use and disclosure of information collected through this Site.
+                            </p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Information We Collect From You.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>We automatically collect information about your Internet connection, including but not limited to:</p>
+                            <ol>
+                              <li>The name of the Internet domain you used to access the Internet and/or the web site from which you linked directly to our Site;</li>
+                              <li>The IP address of your device and the country location of your device;</li>
+                              <li>The connection speed/screen resolution/browser device type used;</li>
+                              <li>The date and time you accessed our Site; and</li>
+                              <li>The pages you visited.</li>
+                            </ol>
+                          </li>
+                          <li>
+                            <p>We use this information we automatically collect about you to monitor the effectiveness of the Site and to consider potential improvements to the Site.</p>
+                          </li>
+                          <li>
+                            <p>We may request that you provide certain personally identifiable information or information specific to your business ("<strong>Personal Information</strong>") to view certain areas of the Site and/or purchase products from the Site. We request that you provide (1) contact information, including but not limited to your name, address, e-mail address and phone number and (2) certain credit card information when making a purchase on the Site. <strong>Your credit card information is encrypted and provided to the credit card issuer for verification and approval. We do not retain any credit card information following verification and approval. At your option, you may save your card for future purchases. This information is maintained on our credit card processor, not on our website.</strong> If you wish to apply for credit with S&amp;S, you may download a credit application, fill it out and submit through the Site. We do not collect any Personal Information about you when you visit the Site unless you give us that information. You may view the products on the Site without registering or providing any Personal Information. You may choose not to provide Personal Information via the Site by not purchasing any products available on the Site.</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Use of Your Personal Information.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>We use your personal information to administer your participation in the Site and to communicate information we think may be of interest to you.</p>
+                          </li>
+                          <li>
+                            <p>To provide you with information, products, or services that you request from us.</p>
+                          </li>
+                          <li>
+                            <p>To fulfill any other purpose for which you provide it.</p>
+                          </li>
+                          <li>
+                            <p>To carry out our obligations and enforce our rights arising from any contracts entered into between you and us, including for billing and collection.</p>
+                          </li>
+                          <li>
+                            <p>In any other way we may describe when you provide the information.</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Sharing Your Information</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>We do not sell your personal information.</p>
+                          </li>
+                          <li>
+                            <p>We may disclose aggregated information about our users, and information that does not identify any individual, without restriction.</p>
+                          </li>
+                          <li>
+                            <p>We may disclose personal information that we collect or you provide as described in this Privacy Policy:</p>
+                            <ul>
+                              <li>To our subsidiaries and affiliates</li>
+                              <li>To contractors, service providers, and other third parties we use to support our business (e.g., we may share your credit card information with the credit card provider to process your purchase). When we use affiliated service providers, we ensure that they maintain appropriate physical, technical and administrative safeguards. If you would like more information about our service providers, please contact us using the contact information in the "Contact Us" section below.</li>
+                              <li>To a buyer or other successor in the event of a merger, divestiture, restructuring, reorganization, dissolution, or other sale or transfer of some or all of our assets, whether as a going concern or as part of bankruptcy, liquidation, or similar proceeding, in which personal information held by us about our Site users is among the assets transferred.</li>
+                              <li>To fulfill the purpose for which you provide it.</li>
+                              <li>For any other purpose disclosed by us when you provide the information.</li>
+                              <li>To comply with any court order, law, or legal process, including to respond to any government or regulatory request.</li>
+                              <li>To enforce or apply our terms of use and other agreements, including for billing and collection purposes.</li>
+                              <li>If we believe disclosure is necessary or appropriate to protect the rights, property, or safety of S&amp;S, our customers, or others (e.g., to detect security breaches and protect against fraud).</li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>How long do we retain personal information?</u></strong>
+                        </p>
+                        <ul>
+                          <li>Subject to applicable laws, we will only retain personal information for as long as necessary to fulfill the identified purposes or as long as required for legal or business purposes.</li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Use of Cookies.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>Like many websites, we use "cookies" and similar tracking technologies to collect information (hereinafter collectively referred to as "cookies"). A cookie is a small data file that we transfer to your computer's hard disk for record-keeping purposes. The use of cookies enables us to enhance your visits to the Site. Cookies are alphanumeric identifiers that are transferred to your computer's hard drive through your web browser. The help portion of the toolbar on most browsers will tell you how to prevent your browser from accepting new cookies, but this may limit your ability to use some features of the Site. Cookies enable us to recognize your browser and provide features that are tailored to your needs. Cookies must be enabled in order for you to log onto the Site. We use them to keep track of your user name and password, but this feature is optional and you may elect not to have your user name and password recognized.</p>
+                          </li>
+                          <li>
+                            <p>We may use information from cookies for purposes of planning and tracking, as well as assessing the technical function of the Site. We may also use cookies to track information about which pages you visit within the Site to help us gauge the effectiveness of the Site or our content. More specifically, we may use:</p>
+                          </li>
+                          <li>
+                            <p>Essential cookies: Some cookies are essential for the operation of our website. If you opt to disable these cookies, you will not be able to access or use all of the features that the website incorporates;</p>
+                          </li>
+                          <li>
+                            <p>Statistics cookies: These cookies enable us to understand how visitors interact with website by collecting and reporting information anonymously.</p>
+                          </li>
+                          <li>
+                            <p>Marketing cookies: We use targeting cookies in order to be able to deliver targeted content.</p>
+                          </li>
+                          <li>
+                            <p>You can set up your computer to inform you whenever a cookie is being sent, or you can disallow cookies in your web browser. If you do choose to disallow cookies, some features of this website may not function, or your ability to choose certain options may be limited. If you would like more information about this practice and your options, you can visit https://www.allaboutcookies.org.</p>
+                          </li>
+                          <li>
+                            <p>Additionally, our use of these cookies is governed by your acceptance of our cookie notification message. Registered users may adjust their cookie preferences under My Account/User Settings.</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Linked Sites.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>We may provide links on the Site to other sites of interest. We do not review or endorse the content of these sites or guarantee that they will abide by this Privacy Policy. Your use of such linked sites is subject to the privacy policies of the providers of those sites. We encourage our users to be aware when they leave our Site and to read the privacy statements of each Site that collects Personal Information.</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Security Measures.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>We have implemented measures designed to secure your personal information from accidental loss and from unauthorized access, use, alteration, and disclosure. The safety and security of your information also depends on you. Where we have given you (or where you have chosen) a password for access to certain parts of our Site, you are responsible for keeping this password confidential. We ask you not to share your password with anyone. Unfortunately, the transmission of information via the internet is not completely secure. Although we do our best to protect your personal information, we cannot guarantee the security of your personal information transmitted to our Site. Any transmission of personal information is at your own risk. We are not responsible for circumvention of any privacy settings or security measures contained on the Site.</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Children and Parents.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>We do not offer or provide products and services for children. If you are under 18, you may not use our Site. We do not intend to collect Personal Information from children. If your child has submitted Personal Information and you would like to request that such information be removed, you may contact us via one of the contact methods described on the Site and we will promptly remove such information and shall not use such information or include it in our database. Users under the age of 18 and their parents or guardians are advised that any Personal Information voluntarily entered by children may be available for public access until we become aware that a user is under the age of 18 and have had a reasonable opportunity to remove the information from the site. If you believe we might have any information from or about a child, please contact our Customer Service Department at 810-547-1646</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Reviewing Information.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>You may review your Personal Information stored in your profile by logging into the Site. You may change your email address or the designated contact person but if you wish to make a change to your company name or address, you will need to contact our Customer Service Department at 810-547-1646. You may contact us via one of the contact methods described in the Terms and Conditions if you wish to discuss any issues regarding your Personal Information.</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Do Not Track.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>Certain web browsers and other devices you may use to access the services may permit you to submit your preference that you do not wish to be "tracked" online. We do not currently commit to responding to "do not track" requests, in part, because no common industry standard for "do not track" has been adopted by industry groups, technology companies, or regulators.</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <p>
+                          <strong><u>Changes.</u></strong>
+                        </p>
+                        <ul>
+                          <li>
+                            <p>We may change this Privacy Policy from time-to-time. If we do make a change, we will post the revised Privacy Policy on this Site. We encourage you to review this Privacy Policy periodically. Questions, comments or complaints about our privacy policies should be submitted to us via the methods described in the Terms and Conditions.</p>
+                          </li>
+                        </ul>
+                      </li>
+                      <p>
+                        <strong><u>California's "Shine the Light" law</u></strong>
+                      </p>
+                      <ul>
+                        <li>
+                          <p>California's "Shine the Light" law (Civil Code Section § 1798.83) permits users of our Site that are California residents to request certain information regarding our disclosure of personal information to third parties for their direct marketing purposes. To make such a request, please send an email to privacy@ssctivewear.com or write us at: 220 Remington Blvd, Bolingbrook, IL 60440, Attention: Customer Service.</p>
+                        </li>
+                      </ul>
+                    </ul>
+                </div>
+
+              </Col>
+            </Row>
+          </Container>
+        </section>
+       
+      </CommonLayout>
+    </>
+  );
+};
+
+export default PrivacyPolicy;
